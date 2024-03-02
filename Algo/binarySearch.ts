f639703a18220598c94a27d9cb6d07f59844fc61
Muneef Altamimi <Munef506@gmail.com>
2024-03-02 15:31:01 +0800

    small change

--- Algo/binarySearch.ts
+++ Algo/binarySearch.ts
@@ -1,17 +1,18 @@
 function binary_search(arr: number[], target: number): number {
   let low = 0;
-  let height = arr.length;
+  let high = arr.length;
 
   do {
-    const m = Math.floor((low + height) / 2);
+    const m = Math.floor((low + high) / 2);
+
     if (arr[m] === target) {
       return m;
     } else if (arr[m] < target) {
       low = m + 1;
     } else {
-      height = m;
+      high = m;
     }
-  } while (low < height);
+  } while (low < high);
 
   return -1;
 }
