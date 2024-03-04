77b35a20f314b84cea167883bb5e98cae7f1da5f
Muneef Altamimi <Munef506@gmail.com>
2024-03-04 16:39:27 +0800

    implement optimized version

--- Algo/BubbleSort.ts
+++ Algo/BubbleSort.ts
@@ -8,24 +8,42 @@
  */
 
 class BubbleSort {
-	constructor() {}
+  constructor() {}
 
-	static sort(array: number[]) {
-		for (let i = 0; i < array.length; i++) {
-			for (let j = i + 1; j < array.length; j++) {
-				if (array[i] > array[j]) {
-					// classic swapping approach
-					// let temp = array[i];
-					// array[i] = array[j];
-					// array[j] = temp;
+  static sort(array: number[]) {
+    for (let i = 0; i < array.length; i++) {
+      for (let j = i + 1; j < array.length; j++) {
+        if (array[i] > array[j]) {
+          // classic swapping approach
+          // let temp = array[i];
+          // array[i] = array[j];
+          // array[j] = temp;
 
-					//ES6 destructuring approach
-					[array[i], array[j]] = [array[j], array[i]];
-				}
-			}
-		}
-		return array;
-	}
+          //ES6 destructuring approach
+          [array[i], array[j]] = [array[j], array[i]];
+        }
+      }
+    }
+    return array;
+  }
 }
 
 console.log(BubbleSort.sort([99, 100, 3, 2, 6, 1])); // [ 1, 2, 3, 6, 99, 100 ]
+
+// ****************************************************************
+// Optimized version
+// ****************************************************************
+function Bubble(arr: number[]): number[] {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        //swap
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
